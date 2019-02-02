6041740a2ae55e1f63588bc7a9d74e357894efb5
Tim Buchwaldt <tim@buchwaldt.ws>
2019-02-02 16:54:19 +0100

    Report firmware status

--- lib/updater.ex
+++ lib/updater.ex
@@ -11,10 +11,13 @@ defmodule Updater do
     slot_a_firmware = Nerves.Runtime.KV.get("a.nerves_fw_version")
     slot_b_firmware = Nerves.Runtime.KV.get("b.nerves_fw_version")
 
+    status = Nerves.Firmware.state().status
+
     data = %{
       active_partition: nerves_fw_active,
       slot_a_firmware: slot_a_firmware,
-      slot_b_firmware: slot_b_firmware
+      slot_b_firmware: slot_b_firmware,
+      status: status
     }
 
     HTTPoison.post("http://" <> @ip <> "/report", Jason.encode(data), [
